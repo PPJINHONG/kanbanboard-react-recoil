780a3993abcc5f217473225103615cfd6243f2d6
ppjinhong <pearljh19@gmail.com>
2022-07-09 02:39:56 +0900

    0709.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,63 @@
 import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { DragDropContext, Droppable,Draggable} from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+display: flex;
+max-width: 500px;
+margin: 0 auto;
+justify-items: center;
+align-items: center;
+height: 100vh;
+`;
+const Boards =styled.div`
+display: grid;
+grid-template-columns: repeat(1,1fr);
+width: 100%;
+
+`;
+const Board = styled.div`
+background-color:white;
+min-height: 300px;
+padding: 30px;
+border-radius: 10px;
+border: 0.5px solid gray;
+`;
+const Card = styled.div`
+background-color: whitesmoke;
+padding: 10px;
+text-align: center;
+margin-bottom:10px;
+`;
 
 function App() {
   const dragend = () => {
-
   };
-
+  const todos =["a","b","c","d","e","f"];
+  
   return (
     <>
     <DragDropContext onDragEnd={dragend}>
-      <div>
+      <Wrapper>
+        <Boards>
     <Droppable droppableId='one'>
-      {(magic) => <ul ref={magic.innerRef}{...magic.droppableProps}>
-        <Draggable draggableId='first' index={0}>
-          {(magic)=>
-          <li ref={magic.innerRef}
-          // {...magic.dragHandleProps}  특정부분 움직임허용
-          {...magic.draggableProps}>  
-           <span{...magic.dragHandleProps}>ooooo</span>
-           helllo
-           </li>}
-            </Draggable>
-        <Draggable draggableId='second' index={1}> 
-         {(magic)=>
-          <li ref={magic.innerRef}
-          {...magic.draggableProps}//전체부분 허용
-          {...magic.dragHandleProps}>
-            fuck</li>}
-            </Draggable>
-        </ul>}
+      {(magic) => <Board ref={magic.innerRef}{...magic.droppableProps}>
+      {todos.map((todo,index)=>(
+        <Draggable draggableId={todo} index={index}>
+        {(magic)=>
+        <Card ref={magic.innerRef}
+         {...magic.dragHandleProps}  
+         {...magic.draggableProps}>  
+                 {todo}
+         </Card>}
+          </Draggable>
+          ))}
+          {magic.placeholder}
+        </Board>}
     </Droppable>
-    </div>
+    </Boards>
+    </Wrapper>
     </DragDropContext>
   </>
   );
